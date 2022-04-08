539b28876582a5dbbe79b11249541f8028e7b002
Rahul Biswakarma <rahul.id39@gmail.com>
2022-04-08 23:06:42 +0530

    Change Framer Motion version from 6.2.9 -> 6.2.8

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,37 @@ import Router from "next/router";
 import "nprogress/nprogress.css";
 import NProgress from "nprogress";
 import SideNav from "/components/sidenav";
+import { motion, AnimatePresence } from "framer-motion";
 
 //Binding events.
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
-    <div className="main-container-algospace">
+    <motion.div
+      key={router.route}
+      initial="pageIniital"
+      animate="pageAnimate"
+      exit="pageExit"
+      className="main-container-algospace"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+        },
+        pageExit: {
+          background: "var(--bg)",
+          opacity: 0,
+        },
+      }}
+    >
       <SideNav />
       <Component {...pageProps} />
-    </div>
+    </motion.div>
   );
 }
 
