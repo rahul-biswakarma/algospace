519716fb39370d730e45a4e8a2c52ab3fc295034
Rahul Biswakarma <rahul.id39@gmail.com>
2022-04-10 00:31:45 +0530

    Added Title

--- pages/tower-of-hanoi/index.js
+++ pages/tower-of-hanoi/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import Head from "next/head";
 import TowerOfHanoi from "../../components/hanoi";
 
 const TowerOfHanoiIndexJs = () => {
   return (
     <>
+      <Head>
+        <title>Tower of Hanoi Visualization | AlgoSpace</title>
+      </Head>
       <TowerOfHanoi />
     </>
   );
