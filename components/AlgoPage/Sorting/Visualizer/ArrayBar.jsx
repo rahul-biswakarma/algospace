b709fdfc43a8975b0f1c00dfbcddcef66824d10a
Rahul Biswakarma <rahul.id39@gmail.com>
2022-09-17 00:17:23 +0530

    Fixed typo in the folder name

---
+++ components/AlgoPage/Sorting/Visualizer/ArrayBar.jsx
@@ -0,0 +1,23 @@
+import * as Colors from "/components/AlgoPage/Searching/SearchingUtils/colors.js";
+const ArrayBar = (props) => {
+  return (
+    <div
+      style={{
+        width: `${props.width}px`,
+        height: `${props.height}px`,
+        backgroundColor: `${
+          props.bgColor == undefined ? Colors.DEFAULT_BG_COLOR : props.bgColor
+        }`,
+        borderColor: `${
+          props.borderColor == undefined
+            ? Colors.DEFAULT_BORDER_COLOR
+            : props.borderColor
+        }`,
+      }}
+      id={props.id}
+      className={`array-bar border-2`}
+    ></div>
+  );
+};
+
+export default ArrayBar;
