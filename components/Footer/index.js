913d3536ef2ddfc887fe0e3e1350c83773cddc75
Rahul Biswakarma <rahul.id39@gmail.com>
2022-09-18 15:08:08 +0530

    Added bottom margin in footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-green-bg text-green font-space flex justify-center p-[1.5rem] mt-gap">
+    <div className="bg-green-bg text-green font-space flex justify-center p-[1.5rem] mt-gap mb-gap">
       Made with ❤️ by{" "}
       <span className="text-blue ml-[15px]">Rahul Biswakarma</span>
     </div>
