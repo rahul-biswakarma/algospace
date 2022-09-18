a31b64c683b1ea62406db64673efa71d4e29f77f
Rahul Biswakarma <rahul.id39@gmail.com>
2022-09-18 16:02:32 +0530

    Added Redirections

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
+  async redirects() {
+    return [
+      {
+        source: "/sorting",
+        destination: "/sorting/bubble-sort",
+        permanent: true,
+      },
+      {
+        source: "/searching",
+        destination: "/searching/linear-search",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
