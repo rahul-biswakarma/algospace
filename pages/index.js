b30be2932999acb59de10689899d50c43e53f672
Rahul Biswakara <rahul.id39@gmail.com>
2022-11-10 15:17:31 +0530

    Fixed client bug

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import Seo from "/components/Seo";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
 
 import TopBar from "/components/TopBar";
 import Terrain from "/components/Terrain";
@@ -12,7 +11,6 @@ import {
 } from "/redux/reducers/pageSlice";
 
 export default function Home() {
-  const router = useRouter();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -24,7 +22,34 @@ export default function Home() {
   });
   return (
     <div>
-      <Seo category="home" id={router.query.algoid} />
+      <NextSeo
+        title="Home | Algospace"
+        description="A algorithm visualizer for understanding algorithms"
+        canonical={`https://algospace.vercel.app/`}
+        openGraph={{
+          url: `https://algospace.vercel.app/`,
+          title: "Home | Algospace",
+          description: "A algorithm visualizer for understanding algorithms",
+          profile: {
+            firstName: "Rahul",
+            lastName: "Biswakarma",
+          },
+          images: [
+            {
+              url: "https://algospace.vercel.app/social.png",
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+              type: "image/jpeg",
+            },
+          ],
+          siteName: "Algospace",
+        }}
+        twitter={{
+          handle: "@rahul_7298",
+          cardType: "summary_large_image",
+        }}
+      />
       <div className="p-gap">
         <div className="relative w-[100%] h-[70vh] border-[1px] border-border-1 overflow-hidden">
           {/* <div className="absolute bottom-0 uppercase font-space text-[13rem] text-[#111] text-center leading-[10rem] tracking-tighter w-[100%] z-10">
